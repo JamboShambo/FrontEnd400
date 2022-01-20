5179ce1635c9d4b9c4a0f185d3a06e2f3cf1dd82
s00189802 <jamiedonohoe2805@gmail.com>
2022-01-20 22:48:15 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import hehfbahfb from "./Footer/footer";
 
 function App() {
+  var LambdaVar1 = "TEST TEST 1";
+  var LambdaVar2 = "TEST TEST 2";
+  var LambdaVar3 = "TEST TEST 3";
+
   return (
     <div className="App">
       <h1>HELLO WORLD!</h1>
+
+      <h1>{LambdaVar1}</h1>
+      <h1>{LambdaVar2}</h1>
+      <h1>{LambdaVar3}</h1>
+      <h1>{hehfbahfb}</h1>
     </div>
   );
 }
---
+++ src/Footer/footer.js
@@ -0,0 +1,3 @@
+const hehfbahfb = "hhesajnbfhabfahbjf";
+
+export default hehfbahfb;
