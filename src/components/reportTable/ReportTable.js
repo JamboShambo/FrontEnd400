760facebdef5e81d2b7efc4731edf09af1f235cd
s00189802 <jamiedonohoe2805@gmail.com>
2022-04-10 00:44:10 +0100

    report table fix

--- src/components/reportTable/ReportTable.js
+++ src/components/reportTable/ReportTable.js
@@ -1,54 +1,47 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import envVars from "../../config";
+import React, { useEffect } from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import ReportTableCell from "../reportTableCell/ReportTableCell";
 
 function ReportTable() {
-  const [getEvents, setGetEvents] = useState([]);
+  var reportTableEvents = JSON.parse(localStorage.getItem("homePageEvents"));
+  var me = reverseArr(reportTableEvents);
 
   useEffect(() => {
-    axios
-      .get(envVars.GetAllEvents, {
-        headers: {
-          "x-api-key": envVars.CustomerApiKeyGateway, // api key for API gateway
-        },
-      })
-      .then((response) => {
-        // response.data.Items.forEach((el) => {
-        //   el.lat.N = parseFloat(el.lat.N);
-        //   el.lng.N = parseFloat(el.lng.N);
-        // });
-        setGetEvents(response.data.events);
-        console.log(response.data.events);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    console.log(reportTableEvents);
+    console.log(me);
   }, []);
 
   var rows = [];
 
+  function reverseArr(input) {
+    var ret = new Array();
+    for (var i = input.length - 1; i >= 0; i--) {
+      ret.push(input[i]);
+    }
+    return ret;
+  }
+
   for (var key = 0; key < 8; key++) {
-    // note: we are adding a key prop here to allow react to uniquely identify each
-    // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-    rows.push(<ReportTableCell getEvents={getEvents[key]} key={key} />);
+    rows.push(<ReportTableCell me={me[key]} key={key} />);
   }
 
   return (
-    <div>
-      <table className="highlight responsive-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>County</th>
-            <th>Date</th>
-          </tr>
-        </thead>
+    <div className="white" style={{}}>
+      <h3>Latest Reports</h3>
+      <div className="white" style={{}}>
+        <table className="highlight responsive-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>County</th>
+              <th>Date</th>
+            </tr>
+          </thead>
 
-        <tbody>{rows}</tbody>
-      </table>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/components/reportTableCell/ReportTableCell.js
+++ src/components/reportTableCell/ReportTableCell.js
@@ -2,16 +2,14 @@ import React from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 
-function ReportTableCell(getEvents) {
-  //sets a state to whether a submited booking has been posted
-
-  //   var event = send;
+function ReportTableCell(reportTableEvents) {
+  console.log(reportTableEvents);
 
   return (
     <tr>
-      {/* <td>{getEvents.getEvents.eventName}</td>
-      <td>{getEvents.getEvents.county}</td>
-      <td>{getEvents.getEvents.eventTime}</td> */}
+      <td>{reportTableEvents.me.eventName}</td>
+      <td>{reportTableEvents.me.county}</td>
+      <td>{reportTableEvents.me.eventTime}</td>
     </tr>
   );
 }
