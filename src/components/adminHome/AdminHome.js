4a50d33176c7544e2e47d59d8424ea91e86b89f6
s00189802 <jamiedonohoe2805@gmail.com>
2022-04-15 15:54:58 +0100

    admin page

--- src/components/adminHome/AdminHome.js
+++ src/components/adminHome/AdminHome.js
@@ -1,18 +1,25 @@
-import React from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import { Row } from "react-materialize";
 import Foot from "../foot/Foot";
+import React from "react";
 
 import AdminPage from "../adminPage/AdminPage";
+import Flotr1 from "../flotr1/Flotr1";
+import Header from "../header/Header";
 
 function AdminHome() {
   return (
     <div>
       <Row style={{ marginBottom: "0%", marginTop: "0%" }}>
+        <Header></Header>
+      </Row>
+      <Row style={{ marginBottom: "0%", marginTop: "0%" }}>
         <AdminPage />
       </Row>
-
+      <Row>
+        <Flotr1></Flotr1>
+      </Row>
       <Row style={{ marginBottom: "0%", marginTop: "0%" }}>
         <Foot></Foot>
       </Row>
